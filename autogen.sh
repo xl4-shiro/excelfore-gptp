97710783823f775fcb3123089b1a611ea82e7f09
Shiro Ninomiya <shiro@excelfore.com>
2020-08-25 18:36:58 +0900

    use GPL COPYING

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 mkdir -p m4
 touch NEWS README AUTHORS ChangeLog
-git archive --format=tar --remote=ssh://git@excelforejp.com:4510/utilities/xl4licenses \
-    master COPYING | tar x
 autoreconf --install
 automake --add-missing > /dev/null 2>&1
 mkdir -p build
